8877324f38c47df31de950bf13bc55b7a6276ac2
dacole <franklynnnolukachukwudalu@gmail.com>
2024-06-22 17:55:07 +0100

    Changed icon and name

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -14,7 +14,7 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void setupWorldTime()  async {
-    WorldTime instance = WorldTime(location: "Berlin", flag: "germany.png", url: "Europe/Berlin");
+    WorldTime instance = WorldTime(location: "Lagos", flag: "nigeria.png", url: "Africa/Lagos");
     await instance.getTime();
     Navigator.pushReplacementNamed(context, "/home", arguments: {
       "location": instance.location,
